dbd1c3065330575caad3e2e0055c627dd8b34793
Sarah Diehl <sarah.diehl@uni.lu>
2015-09-04 12:16:48 +0200

    Use virtualenv in project dir at startup.

--- run.sh
+++ run.sh
@@ -4,7 +4,11 @@ cd `dirname $0`
 
 # If there is a .venv/ directory, assume it contains a virtualenv that we
 # should run this instance in.
-if [ -d .venv ];
+if [ -d /work/projects/galaxy/internal/.venv ];
+then
+    printf "Activating virtualenv at /work/projects/galaxy/internal/.venv\n" $(pwd)
+    . /work/projects/galaxy/internal/.venv/bin/activate
+elif [ -d .venv ];
 then
     printf "Activating virtualenv at %s/.venv\n" $(pwd)
     . .venv/bin/activate
